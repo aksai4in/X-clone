14ae681a2ec9488b5462eb4bc33deaa7d8ea0092
Aidar Kyshtobaev <ak290802@connect.hku.hk>
2024-02-13 21:10:47 +0800

    bug

--- app/(main)/messages/page.tsx
+++ app/(main)/messages/page.tsx
@@ -1,7 +1,4 @@
-import SideNav from "../../ui/home/side-nav";
 import Messages from "../../ui/messages/messages";
-import Notifications from "../../ui/notifications/notifications";
-import Profile from "../../ui/profile/profile";
 
 export default function Page() {
   return <Messages />;
