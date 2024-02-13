5fbcb55b3395632ca3877cac575ad44d71fa1817
Aidar Kyshtobaev <ak290802@connect.hku.hk>
2024-02-13 21:07:21 +0800

    delete unused

--- app/(main)/bookmarks/page.tsx
+++ app/(main)/bookmarks/page.tsx
@@ -1,10 +1,4 @@
-import { getBookmarkedPostIds, getUserByEmail } from "@/app/lib/actions";
 import Bookmarks from "../../ui/bookmarks/bookmarks";
-import SideNav from "../../ui/home/side-nav";
-import Messages from "../../ui/messages/messages";
-import Notifications from "../../ui/notifications/notifications";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 export default async function Page() {
   return <Bookmarks />;
